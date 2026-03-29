9c2ef4857e851728226fbcbf441d365d94f6ee7e
Ali <Debaiki@gmail.com>
2026-03-29 11:28:12 +0200

    Update app routes for landing, engineering, and pricing flow

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import LandingPage from "./pages/LandingPage.jsx";
 import Dashboard from "./pages/Dashboard";
 import RequestDetail from "./pages/RequestDetail";
 import RequestEditor from "./pages/RequestEditor";
-import EngineeringReview from "./pages/EngineeringReview";
 import EngineeringDashboard from "./pages/EngineeringDashboard";
+import EngineeringReview from "./pages/EngineeringReview";
 import PricingDashboard from "./pages/PricingDashboard";
-
 import PricingReview from "./pages/PricingReview";
 
 export default function App() {
@@ -14,12 +13,15 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+
         <Route path="/projects" element={<Dashboard />} />
         <Route path="/new" element={<RequestEditor isNew={true} />} />
         <Route path="/request/:requestId" element={<RequestDetail />} />
         <Route path="/edit/:requestId" element={<RequestEditor />} />
+
         <Route path="/engineering" element={<EngineeringDashboard />} />
         <Route path="/engineering/:requestId" element={<EngineeringReview />} />
+
         <Route path="/pricing" element={<PricingDashboard />} />
         <Route path="/pricing/:requestId" element={<PricingReview />} />
       </Routes>
